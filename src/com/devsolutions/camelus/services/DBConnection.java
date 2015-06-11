dbaff63667988868f7ce3e6824933298055745d2
Alladin El Akhrass <antikanfous@gmail.com>
2015-06-10 22:39:13 -0400

    Added mybatis config file and sql connection provider class

---
+++ src/com/devsolutions/camelus/services/DBConnection.java
@@ -0,0 +1,31 @@
+package com.devsolutions.camelus.services;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DBConnection {
+
+	private static String ressource = "com/devsolutions/camelus/mappers/myBatis.xml";
+	private static InputStream stream;
+	private static SqlSessionFactory sqlSessionFactory;
+
+	static {
+
+		try {
+			stream = Resources.getResourceAsStream(ressource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(stream);
+			sqlSessionFactory.getConfiguration().addMappers(
+					"com/devsolutions/camelus/mappers");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public static SqlSessionFactory getSqlSessionFactory() {
+		return sqlSessionFactory;
+	}
+}
