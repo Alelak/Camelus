d4210e707234a97a0be6271a8aad8928eae6a0ae
Alladin El Akhrass <antikanfous@gmail.com>
2015-06-22 23:44:50 -0400

    New DB config logic + Added gson lib

--- src/com/devsolutions/camelus/services/DBConnection.java
+++ src/com/devsolutions/camelus/services/DBConnection.java
@@ -1,28 +1,37 @@
 package com.devsolutions.camelus.services;
 
-import java.io.InputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.devsolutions.camelus.utils.DBConfig;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
 public class DBConnection {
 
-	private static String ressource = "com/devsolutions/camelus/mappers/myBatis.xml";
-	private static InputStream stream;
+	private static final String ressource = "com/devsolutions/camelus/mappers/myBatis.xml";
 	private static SqlSessionFactory sqlSessionFactory;
 
 	static {
 
 		try {
+			DBConfig dbConfig = getDBConfig();
+			Properties properties = new Properties();
+			properties.setProperty("username", dbConfig.getUsername());
+			properties.setProperty("password", dbConfig.getPassword());
+			properties.setProperty("url", dbConfig.getUrl());
+			Reader reader = Resources.getResourceAsReader(ressource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader,
+					properties);
+			sqlSessionFactory.getConfiguration().addMappers(
+					"com/devsolutions/camelus/mappers");
 
-			if (sqlSessionFactory == null) {
-				stream = Resources.getResourceAsStream(ressource);
-				sqlSessionFactory = new SqlSessionFactoryBuilder()
-						.build(stream);
-				sqlSessionFactory.getConfiguration().addMappers(
-						"com/devsolutions/camelus/mappers");
-			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -31,5 +40,18 @@ public class DBConnection {
 
 	public static SqlSessionFactory getSqlSessionFactory() {
 		return sqlSessionFactory;
+	}
+
+	public static DBConfig getDBConfig() {
+		DBConfig dbConfig = null;
+		try {
+			FileReader fileReader = new FileReader("DBConfig.json");
+			JsonReader jsonReader = new JsonReader(fileReader);
+			Gson gson = new Gson();
+			dbConfig = gson.fromJson(jsonReader, DBConfig.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return dbConfig;
 	}
 }
