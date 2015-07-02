aa2aa5d517f7c7ac49368cce1b08d144db6351d4
Massinissa Amine Aberbache <aberbachemassinissa@gmail.com>
2015-07-02 17:04:44 -0400

    ajout du rapport de commission

---
+++ src/com/devsolutions/camelus/entities/CommissionTV.java
@@ -0,0 +1,40 @@
+package com.devsolutions.camelus.entities;
+
+public class CommissionTV {
+	private long client_id;
+	private String entreprise_name;
+	private double total_sale;
+	private double total_commission;
+
+	public long getClient_id() {
+		return client_id;
+	}
+
+	public void setClient_id(long client_id) {
+		this.client_id = client_id;
+	}
+
+	public String getEntreprise_name() {
+		return entreprise_name;
+	}
+
+	public void setEntreprise_name(String entreprise_name) {
+		this.entreprise_name = entreprise_name;
+	}
+
+	public double getTotal_sale() {
+		return total_sale;
+	}
+
+	public void setTotal_sale(double total_sale) {
+		this.total_sale = total_sale;
+	}
+
+	public double getTotal_commission() {
+		return total_commission;
+	}
+
+	public void setTotal_commission(double total_commission) {
+		this.total_commission = total_commission;
+	}
+}
